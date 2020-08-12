c92210fcd9ade73dce838b84dea244c55f437dbc
justinkrojos <justinkrojos@gmail.com>
2020-08-12 12:41:09 +1200

    Added basic Processor class

---
+++ src/main/java/Processor.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Processor {
+
+    private int currentTime = 0;
+    private ArrayList<SolutionNode> currentTasks;
+
+    Processor() {
+
+    }
+
+    public void getCurrentTime() {
+
+    }
+
+    public void scheduleTask() {
+
+    }
+
+}
