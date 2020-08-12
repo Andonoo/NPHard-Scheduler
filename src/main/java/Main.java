b26055923fea57c1e2f29a9588a108c94c8c7efe
Justin Rojos <justinkrojos@gmail.com>
2020-08-12 19:19:28 +1200

    Skeleton code (#1)
    * Added basic SolutionNode class
    
    * Added basic description to potential methods
    
    * Added basic Processor class
    
    * Added skeleton code to algorithm methods
    
    * Incorporated SolutionNode to skeleton code
    
    * Fixed algorithmic issue with calculating start time
    
    * Added _styling to props

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,88 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
 
+    /*
+    parseInput() will utilise the dot file inputted by the user and parse it to a usable object using GraphStream
+     */
+    public void parseInput() {
+
+    }
+
+    /*
+    executeAlgorithm() will return a valid schedule
+     */
+    public ArrayList<SolutionNode> executeAlgorithm(/* Graph G */) {
+
+        /*
+        Let D be an adjacency list where D{V} is the dependencies of V
+        sortTopologically(G);
+        For all v in V
+            Add output of scheduleByGreedy(v) to ArrayList
+         */
+
+        return null;
+    }
+
+    /*
+    sortTopologically() will return a topological ordering of the vertices in Graph G
+     */
+    public ArrayList<?> sortTopologically(/* Graph G */) {
+
+        /*
+        Let topOrder be an array of vertices/nodes
+        While V is not empty
+            Find a vertex v in V with no outgoing edges
+            Pop v from V
+            Add v to topological order
+        Reverse topOrder
+         */
+
+        return null;
+    }
+
+    /*
+    selectByGreedy will schedule a task based on the greedy heuristic: Earliest Start Time
+     */
+    public SolutionNode scheduleByGreedy(/* Node v */) {
+
+        /*
+        Let P = {Processor one, Processor two, Processor three ...}
+        Processor earliest = one;
+        For p in P
+            If calculateStartTime(p, v) < calculateStartTime(earliest, v)
+                earliest = p;
+        return earliest.scheduleTask(v);
+         */
+
+        return null;
+
+
+    }
+
+    /*
+    calculateStartTime() will return the earliest possible scheduling of task in a specified processor
+     */
+    public void calculateStartTime(/* Processor p, Node v */) {
+
+        /*
+        Let d = D(v)
+        int startTime = p.getCurrentTime();
+        For d' in d
+            Find SolutionNode x that has d'
+            If !p.hasTask(d')
+                startTime = max{startTime, x.processor.currentTime + w(d', v)}
+         */
+    }
+
+    /*
+    printOutput() will read the valid array of SolutionNodes and parse it into the dot output file
+     */
+    public void printOutput() {
+
+    }
 }
---
+++ src/main/java/Processor.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Processor {
+
+    private int _currentTime = 0;
+    private ArrayList<SolutionNode> _currentTasks;
+
+    Processor() {
+
+    }
+
+    public int getCurrentTime() {
+        return _currentTime;
+    }
+
+    public SolutionNode scheduleTask() {
+
+        return null;
+    }
+
+}
