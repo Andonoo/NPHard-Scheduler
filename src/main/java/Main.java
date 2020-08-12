9db7f369821668b10fa8ac490dc6caf1beee9179
justinkrojos <justinkrojos@gmail.com>
2020-08-12 12:35:43 +1200

    Added basic description to potential methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,4 +4,58 @@ public class Main {
         System.out.println("hello world!");
     }
 
+    /*
+    parseInput() will utilise the dot file inputted by the user and parse it to a usable object using GraphStream
+    Input: None
+    Output: None
+     */
+    public void parseInput() {
+
+    }
+
+    /*
+    executeAlgorithm() will return a valid schedule
+    Input: Graph G = (V, E) -> or however GraphStream presents the data
+    Output: An array of SolutionNodes representing a valid schedule
+    Useful methods: sortTopologically(), scheduleByGreedy()
+     */
+    public void executeAlgorithm() {
+    }
+
+    /*
+    sortTopologically() will return a topological ordering of the vertices in Graph G
+    Input: Graph G = (V, E)
+    Output: An array of GraphStream nodes
+     */
+    public void sortTopologically() {
+
+    }
+
+    /*
+    selectByGreedy will schedule a task based on the greedy heuristic: Earliest Start Time
+    Input: A task/GraphStream node
+    Output: None
+    Useful Methods: calculateStartTime()
+     */
+    public void scheduleByGreedy() {
+
+    }
+
+    /*
+    calculateStartTime() will return the earliest possible scheduling of task in a specified processor
+    Input: A task, a processor
+    Output: An int (time) value
+     */
+    public void calculateStartTime() {
+
+    }
+
+    /*
+    printOutput() will read the valid array of SolutionNodes and parse it into the dot output file
+    Input: An array of SolutionNodes representing a valid schedule
+    Output: None
+     */
+    public void printOutput() {
+
+    }
 }
