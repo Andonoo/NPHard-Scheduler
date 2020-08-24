2932580b17fa10f0af0495fffe793dd04fa216b1
Steven <kuanchenho@gmail.com>
2020-08-24 16:43:19 +1200

    Refactor code to use fxml files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,32 +6,22 @@ import io.InputHandler;
 import io.OutputHandler;
 import org.graphstream.graph.implementations.AdjacencyListGraph;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application{
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Scene scene = new Scene(root);
 
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Scheduler visualisation");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
