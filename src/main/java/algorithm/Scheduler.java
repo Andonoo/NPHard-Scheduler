32e85135247b8b884d895df3385f414bbe3b25e4
Steven <kuanchenho@gmail.com>
2020-08-31 00:22:05 +1200

    Update Scheduler interface

--- src/main/java/algorithm/Scheduler.java
+++ src/main/java/algorithm/Scheduler.java
@@ -1,6 +1,9 @@
 package algorithm;
 
 import domain.PartialSchedule;
+import domain.TaskNode;
+
+import java.util.Map;
 
 /**
  * Interface to which the two schedulers (ParallelOptimalScheduler and SequentialOptimalScheduler) will implement.
@@ -14,7 +17,7 @@ public interface Scheduler {
      * @param initialBoundValue
      * @return Returns true if a schedule was found that is shorter than the provided bound value
      */
-    boolean executeBranchAndBoundAlgorithm(double initialBoundValue);
+    boolean executeBranchAndBoundAlgorithm(double initialBoundValue, Map<TaskNode, Double> bottomLevelValues);
 
     /**
      * Gets the schedule which was computed by the branch and bound algorithm. Will return null if no schedule was found
