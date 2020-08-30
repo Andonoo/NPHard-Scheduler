42c0325a45352a279f9e114f9d0125b832f122e7
justinkrojos <justinkrojos@gmail.com>
2020-08-30 21:48:07 +1200

    Added comments to scheduler interface

--- src/main/java/algorithm/Scheduler.java
+++ src/main/java/algorithm/Scheduler.java
@@ -1,14 +1,25 @@
 package algorithm;
 
 import domain.PartialSchedule;
-import domain.TaskNode;
-import org.graphstream.graph.Edge;
-import org.graphstream.graph.Node;
 
-import java.util.*;
-
+/**
+ * Interface to which the two schedulers (ParallelOptimalScheduler and SequentialOptimalScheduler) will implement.
+ * They will inherit the methods described below.
+ */
 public interface Scheduler {
 
-    public boolean executeBranchAndBoundAlgorithm(double initialBoundValue);
-    public PartialSchedule getSolution();
+    /**
+     * Gets the schedule which was computed by the branch and bound algorithm. Will return null if no schedule was found
+     * with a shorter length than the provided initialBoundValue
+     * @param initialBoundValue
+     * @return Returns true if a schedule was found that is shorter than the provided bound value
+     */
+    boolean executeBranchAndBoundAlgorithm(double initialBoundValue);
+
+    /**
+     * Gets the schedule which was computed by the branch and bound algorithm. Will return null if no schedule was found
+     * with a shorter length than the provided initialBoundValue
+     * @return Returns the most optimal completed schedule
+     */
+    PartialSchedule getSolution();
 }
